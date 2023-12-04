7dcf027b64459a8a19944fc675ae133647a1e05e
{{evildeveloper1}} <{{valerasarkisan285@gmail.com}}>
2023-12-04 23:18:02 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Road
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+    public int Lanes { get; set; }
+    public int TrafficLevel { get; set; }
+}
+class Vehicle
+{
+    public int Speed { get; set; }
+    public string Size { get; set; }
+    public string Type { get; set; }
+}
+interface IDriveable
+{
+    void Move();
+    void Stop();
+}
+class TrafficSimulation
+{
+    public void SimulateMovement(Vehicle vehicle, Road road)
+    {
+        if (vehicle.Speed > 0 && road.TrafficLevel < 3)
+        {
+            Console.WriteLine($"The vehicle is moving on the road with {road.Lanes} lanes.");
+        }
+        else
+        {
+            Console.WriteLine("The vehicle is stopped due to heavy traffic or road conditions.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Road road = new Road { Length = 10, Width = 2, Lanes = 2, TrafficLevel = 2 };
+        Vehicle car = new Vehicle { Speed = 60, Size = "Medium", Type = "Car" };
+
+        TrafficSimulation simulator = new TrafficSimulation();
+        simulator.SimulateMovement(car, road);
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+interface IConnectable
+{
+    void Connect(Computer target);
+    void Disconnect(Computer target);
+    void TransferData(Computer target, string data);
+}
+class Computer
+{
+    public string IPAddress { get; set; }
+    public int Power { get; set; }
+    public string OperatingSystem { get; set; }
+}
+class Server : Computer
+{
+}
+class Workstation : Computer
+{
+}
+class Router : Computer
+{
+}
+class Network
+{
+    private List<Computer> computers;
+
+    public Network()
+    {
+        computers = new List<Computer>();
+    }
+
+    public void AddComputer(Computer computer)
+    {
+        computers.Add(computer);
+    }
+
+    public void ConnectComputers(Computer source, Computer target)
+    {
+        if (computers.Contains(source) && computers.Contains(target))
+        {
+            if (source is IConnectable connectableSource)
+            {
+                connectableSource.Connect(target);
+            }
+        }
+    }
+
+    public void DisconnectComputers(Computer source, Computer target)
+    {
+        if (computers.Contains(source) && computers.Contains(target))
+        {
+            if (source is IConnectable connectableSource)
+            {
+                connectableSource.Disconnect(target);
+            }
+        }
+    }
+    public void TransferData(Computer source, Computer target, string data)
+    {
+        if (computers.Contains(source) && computers.Contains(target))
+        {
+            if (source is IConnectable connectableSource)
+            {
+                connectableSource.TransferData(target, data);
+            }
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Network network = new Network();
+
+        Server server = new Server { IPAddress = "192.168.0.1", Power = 1000, OperatingSystem = "Windows Server" };
+        Workstation workstation = new Workstation { IPAddress = "192.168.0.2", Power = 500, OperatingSystem = "Windows 10" };
+        Router router = new Router { IPAddress = "192.168.0.3", Power = 200, OperatingSystem = "RouterOS" };
+
+        network.AddComputer(server);
+        network.AddComputer(workstation);
+        network.AddComputer(router);
+
+        network.ConnectComputers(server, workstation);
+        network.TransferData(server, workstation, "Hello, Workstation!");
+
+        network.DisconnectComputers(server, workstation);
+    }
+}
